5f531df3d27f42a7f5fa17613c10147e7f31bf99
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-04-07 19:08:49 -0300

    feat: add get-event route

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import fastify from "fastify"
-import { createEvent } from "./routes/create-event"
 import {
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from "fastify-type-provider-zod"
+import { createEvent } from "./routes/create-event"
 import { registerForEvent } from "./routes/register-for-event"
+import { getEvent } from "./routes/get-event"
 
 export const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -14,6 +15,7 @@ app.setSerializerCompiler(serializerCompiler)
 
 app.register(createEvent)
 app.register(registerForEvent)
+app.register(getEvent)
 
 app.listen({ port: 8080 }).then(() => {
   console.log("Server listening on port 8080!")
